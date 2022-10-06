3e9e523b86bbd105c7b746b376bd8b1759ba0f25
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-10-05 22:27:21 -0400

    implementar guardar numero secreto mayor a 4 digitos

--- src/toros_y_vacas.js
+++ src/toros_y_vacas.js
@@ -4,7 +4,7 @@ class ToroVaca{
 
     guardarCodigo(cadena)
     {
-        if(cadena.length>=4)
+        if(cadena.length>=4 && cadena.length<=6)
         {
             this._codigoSecreto=cadena;
         return this._codigoSecreto;
--- src/toros_y_vacas.spec.js
+++ src/toros_y_vacas.spec.js
@@ -9,5 +9,10 @@ describe("", () => {
       expect(resultado).toEqual("23456");
     });
 
-   
+    it("deberia guardar el numero secreto mayor a 4 digitos y menor a 6", () => {
+        const resultado = tv.guardarCodigo("12345");
+  
+        expect(resultado).toEqual("12345");
+      });
+
   });
