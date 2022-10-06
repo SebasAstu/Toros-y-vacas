90b3e8c71190e333c1d684a2fe96fcd97c1ce92a
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-10-06 09:47:20 -0400

    implementar: interfaz de Ingresar codigo secreto de digitos

--- src/presenter.js
+++ src/presenter.js
@@ -1,30 +1,30 @@
-import sumar from "./sumador";
-import multiplicar from "./multiplicador";
+import ToroVaca from "./toros_y_vacas.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+let tv = new ToroVaca();
+
+const codigoSecreto = document.querySelector("#codigo-secreto");
+const btnIniciar = document.querySelector("#btn-iniciar");
+const formGuardar = document.querySelector("#guardar-form");
 const div = document.querySelector("#resultado-div");
 
-const firstMul = document.querySelector("#primer-numeroMul");
-const secondMul = document.querySelector("#segundo-numeroMul");
-const formMul =document.querySelector("#multiplicar-form");
-const divMul= document.querySelector("#resultado-divMul");
+//const firstMul = document.querySelector("#primer-numeroMul");
+//const secondMul = document.querySelector("#segundo-numeroMul");
+//const formMul =document.querySelector("#multiplicar-form");
+//const divMul= document.querySelector("#resultado-divMul");
 
-form.addEventListener("submit", (event) => {
+formGuardar.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const codigo_secreto = tv.guardarCodigo(codigoSecreto.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" +codigo_secreto + "</p>";
 });
 
-formMul.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const firstNumberMul = Number.parseInt(firstMul.value);
-  const secondNumberMul = Number.parseInt(secondMul.value);
-
-  divMul.innerHTML = "<p>" + multiplicar(firstNumberMul, secondNumberMul) + "</p>";
-});
+//formMul.addEventListener("submit", (event) => {
+//  event.preventDefault();
+//
+//  const firstNumberMul = Number.parseInt(firstMul.value);
+//  const secondNumberMul = Number.parseInt(secondMul.value);
+//
+//  divMul.innerHTML = "<p>" + multiplicar(firstNumberMul, secondNumberMul) + "</p>";
+//});
