09dd2f17f71cd4d1a80ecde40708d5e0955824dd
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-10-11 10:00:46 -0400

    "resolucion de tests"

--- src/toros_y_vacas.spec.js
+++ src/toros_y_vacas.spec.js
@@ -64,13 +64,13 @@ describe("guardar codigo secreto", () => {
     it("deberia devolver (!) si algun digito coincide con el codigo secreto", () => {
       juego.guardarCodigoSecreto("23456");
       const resultado = juego.verificarCodigo("20897");
-      expect(resultado).toEqual("!");
+      expect(resultado).toEqual("!#");
     });
 
     it("no deberia devolver (!) si si ningun digito coincide con el codigo secreto", () => {
       juego.guardarCodigoSecreto("23456");
       const resultado = juego.verificarCodigo("10789");
-      expect(resultado).toEqual("");
+      expect(resultado).toEqual("##");
     });
   });
 
@@ -84,12 +84,12 @@ describe("guardar codigo secreto", () => {
     it("deberia devolver (*) si algun digito coincide en con el codigo secreto pero no se enecuentra en la posicion correcta", () => {
       juego.guardarCodigoSecreto("23456");
       const resultado = juego.verificarCodigo("12897");
-      expect(resultado).toEqual("*");
+      expect(resultado).toEqual("#*#");
     });
     it("no deberia devolver (*) si si ningun digito coincide con el codigo secreto", () => {
       juego.guardarCodigoSecreto("23456");
       const resultado = juego.verificarCodigo("10789");
-      expect(resultado).toEqual("");
+      expect(resultado).toEqual("##");
     });
   });
 
